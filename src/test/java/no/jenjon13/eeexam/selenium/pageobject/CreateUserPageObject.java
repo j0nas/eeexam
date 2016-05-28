bcfb6d11a244d946b103de38ed68a1e2c6401ce4
Jonas Jensen <jonas.jensen@msn.com>
2016-05-28 11:49:48 +0200

    e7: testCreateUserFailDueToPasswordMismatch

---
+++ src/test/java/no/jenjon13/eeexam/selenium/pageobject/CreateUserPageObject.java
@@ -0,0 +1,11 @@
+package no.jenjon13.eeexam.selenium.pageobject;
+
+import org.openqa.selenium.WebDriver;
+
+public class CreateUserPageObject extends BasePageObject {
+    public CreateUserPageObject(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
--- src/test/java/no/jenjon13/eeexam/selenium/test/WebPageIT.java
+++ src/test/java/no/jenjon13/eeexam/selenium/test/WebPageIT.java
@@ -3,10 +3,7 @@ package no.jenjon13.eeexam.selenium.test;
 import no.jenjon13.eeexam.selenium.conf.Config;
 import no.jenjon13.eeexam.selenium.conf.JBossUtil;
 import no.jenjon13.eeexam.selenium.pageobject.HomePageObject;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,6 +11,7 @@ import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
 
@@ -101,5 +99,37 @@ public class WebPageIT {
 
         homePageObject.waitForPageToLoad();
         assertTrue(isOnLoginPage());
+    }
+
+    @Test
+    public void testCreateUserFailDueToPasswordMismatch() throws Exception {
+        navigateToLoginPage();
+
+        By byBtnCreateNewUser = By.id("loginForm:btnCreateNewUser");
+        WebElement createNewUserWebElement = driver.findElement(byBtnCreateNewUser);
+        createNewUserWebElement.click();
+        homePageObject.waitForPageToLoad();
+
+        String currentUrl = driver.getCurrentUrl();
+
+        String[] fields = new String[] {"username", "password1", "password2", "firstname", "lastname"};
+        for (String field : fields) {
+            By byTxtField = By.id("userform:" + field);
+            WebElement txtWebElement = driver.findElement(byTxtField);
+            txtWebElement.sendKeys(field);
+        }
+
+        By bySelectCountry = By.id("userform:country");
+        WebElement selectWebElement = driver.findElement(bySelectCountry);
+        Select select = new Select(selectWebElement);
+        select.selectByVisibleText("Norway");
+
+        By byBtnSubmit = By.id("userform:submit");
+        WebElement submitWebElement = driver.findElement(byBtnSubmit);
+        submitWebElement.click();
+        homePageObject.waitForPageToLoad();
+
+        String newUrl = driver.getCurrentUrl();
+        Assert.assertTrue(newUrl.equals(currentUrl));
     }
 }
